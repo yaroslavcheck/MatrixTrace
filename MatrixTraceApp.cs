1dce690c03646574ebe259640f10eca2ff5e1527
admin <admin@DESKTOP-U232V43>
2022-12-17 17:55:49 +0300

    commit 2

---
+++ MatrixTraceApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class MatrixTraceApp
+{
+    public static void Main()
+    {
+        Console.WriteLine("FoxMinded C# course. Task 02 - MatrixTrace. Student: Yaroslav Korneyko\nPlease input matrix dimensions by spaces:");
+
+        string inputWord = Console.ReadLine() ?? "";
+        string[] dimensions = inputWord.Split(" ");
+        var matrixTrace = new MatrixTraceServise();
+
+        string[,] matrix = matrixTrace.CreateMatrix(Convert.ToInt32(dimensions[0]), Convert.ToInt32(dimensions[1]));
+        Console.WriteLine($"\nMatrix:");
+        matrixTrace.PrintMatrix(matrix);
+        
+        Console.WriteLine($"Matrix Trace: {matrixTrace.MatrixTrace(matrix)}");
+    }
+}
---
+++ MatrixTraceServise.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MatrixTraceServise
+{
+    public string[,] CreateMatrix(int length, int width)
+    {
+        int[] size = new int[] { length, width };
+        string[,] matrix = new string[size[0], size[1]];
+
+        for (int i = 0; i < size[0]; i++)
+        {
+            for (int j = 0; j < size[1]; j++)
+            {
+                Random elementOfMatrix = new Random();
+                int el = elementOfMatrix.Next(1, 100);
+                matrix[i, j] = Convert.ToString(el);
+            }
+        }
+
+        return matrix;
+    }
+    
+    public void PrintMatrix(string[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++) 
+            { 
+                if (i == j) 
+                { 
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                else 
+                { 
+                    Console.ForegroundColor = ConsoleColor.White; 
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+            } 
+            Console.WriteLine();
+        }
+    }
+
+    public string MatrixTrace(string[,] matrix)
+    {
+        int answerMatrixTrace = 0;
+
+        if(matrix.GetLength(0) <= matrix.GetLength(1))
+        { 
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                answerMatrixTrace += Convert.ToInt32(matrix[i, i]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                answerMatrixTrace += Convert.ToInt32(matrix[i, i]);
+            }
+        }
+
+        return Convert.ToString(answerMatrixTrace);
+    }
+}
